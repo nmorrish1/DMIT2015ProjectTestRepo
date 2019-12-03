b2fa6771039c40b178fac7bb826b470ceb537033
Nick Morrish <nmorrish1@studentmail.nait.ca>
2019-12-03 11:09:13 -0800

    Test Commit

--- dmit2015-project-nmorrish1/src/main/java/security/service/RoleBean.java
+++ dmit2015-project-nmorrish1/src/main/java/security/service/RoleBean.java
@@ -1,10 +1,20 @@
 package security.service;
 
+import javax.ejb.Lock;
+import javax.ejb.LockType;
 import javax.ejb.Singleton;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Singleton
 //@Interceptors({LoginGroupSecurityInterceptor.class})
 public class RoleBean {
+	@PersistenceContext
+	private EntityManager entityManager;
 	
+	@Lock(LockType.WRITE)
+	public void add(String groupname) {
+		
+	}
 
 }
