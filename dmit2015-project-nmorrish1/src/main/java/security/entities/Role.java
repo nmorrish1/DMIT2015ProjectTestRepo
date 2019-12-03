0ba6e1d77acc8e369a785b99e8752a736a0e0770
Nick Morrish <nmorrish1@studentmail.nait.ca>
2019-12-03 12:48:52 -0800

    Updated Project

---
+++ dmit2015-project-nmorrish1/src/main/java/security/entities/Role.java
@@ -0,0 +1,39 @@
+package security.entities;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQuery;
+import javax.validation.constraints.NotBlank;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+import lombok.Data;
+
+@XmlRootElement(name = "Role")
+@Data
+@Entity
+@NamedQuery(name="Roles.findAll", query="SELECT r FROM Role r")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Role implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@NotBlank(message="Enter a role name")
+	@Column(length=64, unique=true, nullable=false)
+	private String roleName;
+	
+	@XmlTransient
+	@ManyToMany(mappedBy="roles")
+	private List<User> users;
+}
--- dmit2015-project-nmorrish1/src/main/java/security/service/RoleBean.java
+++ dmit2015-project-nmorrish1/src/main/java/security/service/RoleBean.java
@@ -1,9 +1,13 @@
 package security.service;
 
+import java.util.List;
+
 import javax.ejb.Lock;
 import javax.ejb.LockType;
 import javax.ejb.Singleton;
+import javax.management.relation.Role;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 
 @Singleton
@@ -13,8 +17,52 @@ public class RoleBean {
 	private EntityManager entityManager;
 	
 	@Lock(LockType.WRITE)
-	public void add(String groupname) {
+	public void add(String roleName) {
 		
+		if(findRoleByName(roleName) != null) {
+			throw new RuntimeException("The group name " + roleName + " already exists");
+		}
+		Role newRole = new Role(roleName, null);
+		newRole.setRoleName(roleName);
+		entityManager.persist(newRole);
+	}
+	
+	public void update(Role existingRole) {
+		entityManager.merge(existingRole);
+		entityManager.flush();
+	}
+	
+	public void delete(Role existingRole) {
+		if (entityManager.contains(existingRole)) {
+			existingRole = entityManager.merge(existingRole);
+			entityManager.remove(existingRole);
+		}
+	}
+	
+	public Role findRoleById(Long roleId) {
+		
+		return entityManager.find(Role.class, roleId);
+	}
+	
+	public Role findRoleByName(String roleName) {
+		Role queryResult = null;
+		
+		try {
+			queryResult = entityManager.createQuery(
+					"SELECT r FROM Role r WHERE r.roleName := roleNameVale "
+					, Role.class)
+					.setParameter("roleNameVale", roleName)
+					.getSingleResult();
+		} catch (NoResultException e) {
+			queryResult = null;
+		}
+		
+		return queryResult;
+	}
+	
+	@Lock(LockType.READ)
+	public List<Role> list(){
+		return entityManager.createQuery("SELECT r FROM Role r ORDER BY r.roleName", Role.class).getResultList();
 	}
 
 }
